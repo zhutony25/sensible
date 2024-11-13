dbfe2036e9a8c2832fe0b655a5b8a988756bfe0b
Tony Zhu <zhu.tony@utexas.edu>
2024-11-12 18:29:53 -0600

    Added firebase --> still need to implement w/current pages
    com.specmed.sensible

--- app/frontend/LoginPage.js
+++ app/frontend/LoginPage.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { FontAwesome, MaterialIcons } from '@expo/vector-icons';
 
+import auth from '@react-native-firebase/auth';
+
 export default function LoginPage({ navigation }) {
   return (
     <View style={styles.container}>
